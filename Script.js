f4aed42d83658b2216587aeaa4e0e5c5676debf6
hiri-ya <hawimek46@gmail.com>
2026-05-17 06:22:09 +0300

    Add scrollProducts and addToCart functions

--- Script.js
+++ Script.js
@@ -1,1 +1,13 @@
+function scrollProducts(){
 
+  document.getElementById("products").scrollIntoView({
+    behavior:"smooth"
+  });
+
+}
+
+function addToCart(){
+
+  alert("Product added to cart!");
+
+}
